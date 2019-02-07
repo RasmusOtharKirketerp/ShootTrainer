4052d261942d99875aa9111bd663e56719d11459
RASMUS OTHAR KIRKETERP <rasmusothar@gmail.com>
2019-02-07 23:53:56 +0100

    + and - adds cirkles game adds one cirkle if you kille them all scoreboard added

--- ShootTrainer/src/shootTrainer/Main.java
+++ ShootTrainer/src/shootTrainer/Main.java
@@ -6,6 +6,8 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.RenderingHints;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
@@ -14,185 +16,256 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Main extends JPanel
-{
-	public Main()
-	{
-		// initTargetsList();
-		initRandomTargetList();
-	}
-
-	private static final long serialVersionUID = 1L;
-
-	public static int getScreenWorkingWidth()
-	{
-		return java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width;
-	}
-
-	public static int getScreenWorkingHeight()
-	{
-		return java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height;
-	}
-
-	public static ArrayList<Target> allTargets = new ArrayList<Target>();
-
-	static public void initRandomTargetList()
-	{
-		allTargets.removeAll(allTargets);
-		int count = 15;
-
-		//
-		for (int i = 1; i <= count; i++)
-		{
-			Target newTarget = new Target();
-			allTargets.add(newTarget);
-		}
-
-	}
-
-	static public void initTargetsList()
-	{
-		allTargets.removeAll(allTargets);
-
-		// test case #1 - Stand still
-		Target newTarget = new Target(new Point(30, 400), 30, Color.GREEN);
-		newTarget.setDeltaMove(new Point(0, 0));
-		allTargets.add(newTarget);
-
-		// test case #2 - move
-		Target newTarget2 = new Target(new Point(100, 100), 100, Color.GREEN);
-		newTarget2.setDeltaMove(new Point(0, 1));
-		allTargets.add(newTarget2);
-
-		// test case #3 - stand still and change to bigger size
-		Target newTarget3 = new Target(new Point(400, 400), 10, Color.GREEN);
-		newTarget3.setDeltaMove(new Point(0, 0));
-		newTarget3.setMaxRadius(60);
-		newTarget3.setMinRadius(10);
-		newTarget3.setDeltaRadius(1.005);
-		allTargets.add(newTarget3);
-
-		// test case #3.1 - stand still and change to smaller size
-		Target newTarget5 = new Target(new Point(600, 800), 100, Color.GREEN);
-		newTarget5.setDeltaMove(new Point(0, 0));
-		newTarget5.setMaxRadius(190);
-		newTarget5.setMinRadius(10);
-		newTarget5.setDeltaRadius(0.995);
-		allTargets.add(newTarget5);
-
-		// test case #4 - move and change size
-		Target newTarget4 = new Target(new Point(400, 600), 100, Color.GREEN);
-		newTarget4.setDeltaMove(new Point(2, -1));
-		newTarget4.setMaxRadius(130);
-		newTarget4.setMinRadius(10);
-		newTarget4.setDeltaRadius(1.003);
-		allTargets.add(newTarget4);
-
-		// test case ## - move and change size
-		Target newTarget6 = new Target(new Point(1100, 600), 50, Color.MAGENTA);
-		newTarget6.setDeltaMove(new Point(-2, 0));
-		newTarget6.setMaxRadius(230);
-		newTarget6.setMinRadius(10);
-		newTarget6.setDeltaRadius(1.003);
-		allTargets.add(newTarget6);
-
-	}
-
-	static LokalKoordinatsystem aScreen = new LokalKoordinatsystem(getScreenWorkingWidth(), getScreenWorkingHeight());
-
-	public void paint(Graphics g)
-	{
-		super.paint(g);
-
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-		this.setBackground(Color.black);
-
-		for (Target target : allTargets)
-		{
-
-			// remove target if it is too close to border
-			if (target.getDrawP().x < 100) target.dead(true);
-			if (target.getDrawP().x > getScreenWorkingWidth() - 100) target.dead(true);
-
-			if (target.getDrawP().y < 100) target.dead(true);
-			if (target.getDrawP().y > getScreenWorkingHeight() - 100) target.dead(true);
-
-			target.draw(g2d);
-		}
-
-		g.setColor(Color.blue);
-		g.drawRect(100, 100, getScreenWorkingWidth() - 200, getScreenWorkingHeight() - 200);
-		drawScoreBoard(g);
-		drawLinesToTarges(g);
-
-	}
-
-	private void drawScoreBoard(Graphics g)
-	{
-		int i = 1;
-		for (Target target : allTargets)
-		{
-			g.setColor(target.getC());
-			g.fillArc(30 * i, 30, 30, 30, 0, 360);
-			i++;
-		}
-
-	}
-
-	private void drawLinesToTarges(Graphics g)
-	{
-		for (Target target : allTargets)
-		{
-			g.setColor(Color.GRAY);
-			g.drawLine(100, 100, target.getDrawP().x, target.getDrawP().y);
-			g.drawLine(getScreenWorkingWidth() - 100, getScreenWorkingHeight() - 100, target.getDrawP().x,
-					target.getDrawP().y);
-			g.drawLine(100, getScreenWorkingHeight() - 100, target.getDrawP().x, target.getDrawP().y);
-			g.drawLine(getScreenWorkingWidth() - 100, 100, target.getDrawP().x, target.getDrawP().y);
-		}
-
-	}
-
-	public static void main(String[] args) throws InterruptedException
-	{
-		JFrame frame = new JFrame("ShootTrainer");
-		Main mainScreen = new Main();
-
-		frame.add(mainScreen);
-		frame.setSize(aScreen.maxX, aScreen.maxY);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.addMouseListener(ma);
-
-		while (true)
-		{
-			// Thread.sleep(10);
-			mainScreen.repaint();
-		}
-
-	}
-
-	static public MouseAdapter ma = new MouseAdapter()
 	{
 
-		public void mousePressed(MouseEvent evt)
-		{
-			if ((evt.getModifiers() & InputEvent.BUTTON1_MASK) != 0)
-			{
-				for (Target target : allTargets)
-				{
-					target.isHitEvent(evt);
+		private static int maxTargets = 38;
+		public static int numberOfTargets = 1;
+		public static int totalNumberOfTargetIsHit = 0;
 
-				}
-
-			}
-			if ((evt.getModifiers() & InputEvent.BUTTON3_MASK) != 0)
+		public Main()
 			{
 				// initTargetsList();
 				initRandomTargetList();
+			}
+
+		private static final long serialVersionUID = 1L;
+
+		public static int getScreenWorkingWidth()
+			{
+				return java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width;
+			}
+
+		public static int getScreenWorkingHeight()
+			{
+				return java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height;
+			}
+
+		public static ArrayList<Target> allTargets = new ArrayList<Target>();
+
+		static public void initRandomTargetList()
+			{
+				allTargets.removeAll(allTargets);
+				int count = numberOfTargets;
+				totalNumberOfTargetIsHit = 0;
+
+				//
+				for (int i = 1; i <= count; i++)
+					{
+						Target newTarget = new Target();
+						allTargets.add(newTarget);
+					}
 
 			}
-		}
-	};
-}
+
+		static public void initTargetsList()
+			{
+				allTargets.removeAll(allTargets);
+
+				// test case #1 - Stand still
+				Target newTarget = new Target(new Point(30, 400), 30, Color.GREEN);
+				newTarget.setDeltaMove(new Point(0, 0));
+				allTargets.add(newTarget);
+
+				// test case #2 - move
+				Target newTarget2 = new Target(new Point(100, 100), 100, Color.GREEN);
+				newTarget2.setDeltaMove(new Point(0, 1));
+				allTargets.add(newTarget2);
+
+				// test case #3 - stand still and change to bigger size
+				Target newTarget3 = new Target(new Point(400, 400), 10, Color.GREEN);
+				newTarget3.setDeltaMove(new Point(0, 0));
+				newTarget3.setMaxRadius(60);
+				newTarget3.setMinRadius(10);
+				newTarget3.setDeltaRadius(1.005);
+				allTargets.add(newTarget3);
+
+				// test case #3.1 - stand still and change to smaller size
+				Target newTarget5 = new Target(new Point(600, 800), 100, Color.GREEN);
+				newTarget5.setDeltaMove(new Point(0, 0));
+				newTarget5.setMaxRadius(190);
+				newTarget5.setMinRadius(10);
+				newTarget5.setDeltaRadius(0.995);
+				allTargets.add(newTarget5);
+
+				// test case #4 - move and change size
+				Target newTarget4 = new Target(new Point(400, 600), 100, Color.GREEN);
+				newTarget4.setDeltaMove(new Point(2, -1));
+				newTarget4.setMaxRadius(130);
+				newTarget4.setMinRadius(10);
+				newTarget4.setDeltaRadius(1.003);
+				allTargets.add(newTarget4);
+
+				// test case ## - move and change size
+				Target newTarget6 = new Target(new Point(1100, 600), 50, Color.MAGENTA);
+				newTarget6.setDeltaMove(new Point(-2, 0));
+				newTarget6.setMaxRadius(230);
+				newTarget6.setMinRadius(10);
+				newTarget6.setDeltaRadius(1.003);
+				allTargets.add(newTarget6);
+
+			}
+
+		static LokalKoordinatsystem aScreen = new LokalKoordinatsystem(getScreenWorkingWidth(),
+				getScreenWorkingHeight());
+
+		public void paint(Graphics g)
+			{
+				super.paint(g);
+
+				Graphics2D g2d = (Graphics2D) g;
+				g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+				this.setBackground(Color.black);
+				totalNumberOfTargetIsHit = 0;
+
+				for (Target target : allTargets)
+					{
+
+						// remove target if it is too close to border
+						if (target.getDrawP().x < 100)
+							target.closeToBorder(true);
+						if (target.getDrawP().x > getScreenWorkingWidth() - 100)
+							target.closeToBorder(true);
+
+						if (target.getDrawP().y < 100)
+							target.closeToBorder(true);
+						if (target.getDrawP().y > getScreenWorkingHeight() - 100)
+							target.closeToBorder(true);
+
+						target.draw(g2d);
+						if (target.getShootsLeft() <= 0)
+							{
+								totalNumberOfTargetIsHit++;
+							}
+
+					}
+				if (totalNumberOfTargetIsHit == numberOfTargets)
+					{
+						numberOfTargets += 1;
+						initRandomTargetList();
+					}
+
+				g.setColor(Color.blue);
+				g.drawRect(100, 100, getScreenWorkingWidth() - 200, getScreenWorkingHeight() - 200);
+				drawScoreBoard(g);
+				// drawLinesToTarges(g);
+
+			}
+
+		private void drawScoreBoard(Graphics g)
+			{
+				int i = 0;
+				int s = 45;
+
+				for (Target target : allTargets)
+					{
+						g.setColor(target.getC());
+						g.fillArc(s * i + 100, 20, s - 2, s - 2, 0, 360);
+						i++;
+					}
+				g.setColor(Color.darkGray);
+				for (int theRest = i; theRest < maxTargets; theRest++)
+					{
+						g.drawArc(s * theRest + 100, 20, s - 2, s - 2, 0, 360);
+					}
+
+			}
+
+		@SuppressWarnings("unused")
+		private void drawLinesToTarges(Graphics g)
+			{
+				for (Target target : allTargets)
+					{
+						g.setColor(Color.GRAY);
+						g.drawLine(100, 100, target.getDrawP().x, target.getDrawP().y);
+						g.drawLine(getScreenWorkingWidth() - 100, getScreenWorkingHeight() - 100, target.getDrawP().x,
+								target.getDrawP().y);
+						g.drawLine(100, getScreenWorkingHeight() - 100, target.getDrawP().x, target.getDrawP().y);
+						g.drawLine(getScreenWorkingWidth() - 100, 100, target.getDrawP().x, target.getDrawP().y);
+					}
+
+			}
+
+		public static void main(String[] args) throws InterruptedException
+			{
+				JFrame frame = new JFrame("ShootTrainer");
+				Main mainScreen = new Main();
+
+				frame.add(mainScreen);
+				frame.setSize(aScreen.maxX, aScreen.maxY);
+				frame.setVisible(true);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.addMouseListener(ma);
+				frame.addKeyListener(myKL);
+
+				while (true)
+					{
+						mainScreen.repaint();
+					}
+
+			}
+
+		static public KeyListener myKL = new KeyListener()
+			{
+
+				@Override
+
+				public void keyTyped(KeyEvent e)
+					{
+
+						System.out.println("Code : " + e.getKeyCode());
+						System.out.println("Char : " + e.getKeyChar());
+						if (e.getKeyChar() == '+')
+							{
+								numberOfTargets += 1;
+								if (numberOfTargets > maxTargets)
+									numberOfTargets = maxTargets;
+								initRandomTargetList();
+							}
+						if (e.getKeyChar() == '-')
+							{
+								numberOfTargets -= 1;
+								initRandomTargetList();
+							}
+
+					}
+
+				@Override
+				public void keyPressed(KeyEvent arg0)
+					{
+						// TODO Auto-generated method stub
+
+					}
+
+				@Override
+				public void keyReleased(KeyEvent arg0)
+					{
+						// TODO Auto-generated method stub
+
+					}
+
+			};
+
+		static public MouseAdapter ma = new MouseAdapter()
+			{
+
+				public void mousePressed(MouseEvent evt)
+					{
+						if ((evt.getModifiers() & InputEvent.BUTTON1_MASK) != 0)
+							{
+								for (Target target : allTargets)
+									{
+										target.isHitEvent(evt);
+
+									}
+
+							}
+						if ((evt.getModifiers() & InputEvent.BUTTON3_MASK) != 0)
+							{
+								// initTargetsList();
+								initRandomTargetList();
+
+							}
+					}
+			};
+	}
--- ShootTrainer/src/shootTrainer/Target.java
+++ ShootTrainer/src/shootTrainer/Target.java
@@ -7,258 +7,273 @@ import java.awt.event.MouseEvent;
 import java.util.Random;
 
 public final class Target
-{
-	private Point drawP;
-
-	private Point deltaMove;
-	// Radius attributes
-	private double radius;
-	private double deltaRadius;
-	private int minRadius;
-	private int maxRadius = 200;
-
-	private int hitsToKillTarget = 2;
-	private int hitsSoFar = 0;
-
-	private Color c;
-
-	public Color getC()
 	{
-		return c;
+		private Point drawP;
+
+		private Point deltaMove;
+		// Radius attributes
+		private double radius;
+		private double deltaRadius;
+		private int minRadius;
+		private int maxRadius = 200;
+
+		private int hitsToKillTarget = 2;
+		private int hitsSoFar = 0;
+
+		public int getShootsLeft()
+			{
+				return (hitsToKillTarget - hitsSoFar);
+			}
+
+		private Color c;
+
+		public Color getC()
+			{
+				return c;
+			}
+
+		static public final int randInt(int min, int max)
+			{
+				Random rand = new Random();
+				if (max <= 0)
+					{
+						max = 1;
+					}
+				int randomNum = rand.nextInt((max - min) + 1) + min;
+				return randomNum;
+			}
+
+		// Constructor
+		public Target(Point inP, int radius, Color c)
+			{
+				super();
+				init(inP, radius, c);
+			}
+
+		// Constructor - no input = random
+		public Target()
+			{
+				// generate random positions
+				c = Color.green;
+				deltaRadius = 1;
+				minRadius = 10;
+				maxRadius = randInt(10, 200);
+				deltaMove = new Point(0, 0);
+
+				drawP = new Point(randInt(300, 1300), randInt(200, 700));
+				deltaMove = new Point(randInt(1, 4) - 2, randInt(1, 4) - 2);
+				radius = randInt(10, 250);
+				if (randInt(1, 10) >= 5)
+					{
+						deltaRadius = 1.0005;
+						maxRadius = randInt(10, 50);
+						;
+					} else
+					{
+						deltaRadius = 0.99;
+					}
+				hitsSoFar = 0;
+				hitsToKillTarget = 1;
+			}
+
+		// Methods
+
+		// Getters
+		public Point getDrawP()
+			{
+				return drawP;
+			}
+
+		public void changeSizeOfTarget()
+			{
+
+				if (deltaRadius != 1)
+					{
+						this.radius *= deltaRadius;
+					}
+				if (radius > maxRadius)
+					{
+						radius = maxRadius;
+					}
+				if (radius < minRadius)
+					{
+						radius = minRadius;
+					}
+
+			}
+
+		public void debugDetermineIfTargetIsHit(int shootX, int shootY, boolean retVal)
+			{
+				System.out.print("Shoot(" + shootX + "," + shootY + ")");
+				if (retVal)
+					{
+						System.out.println(" Hit! Radius :" + radius + " ");
+					} else
+					{
+						System.out.println(" No hit...");
+					}
+
+				System.out.println("Distance : " + drawP.distance(shootY, shootY));
+
+			}
+
+		public boolean determineIfTargetIsHit(int shootX, int shootY)
+			{
+				boolean retVal = false;
+
+				double distance = drawP.distance(shootX, shootY);
+				if (distance < radius)
+					{
+						retVal = true;
+					} else
+					{
+						retVal = false;
+					}
+
+				// debugDetermineIfTargetIsHit(shootX, shootY, retVal);
+
+				return retVal;
+			}
+
+		public void drawDebugInfo(Graphics2D g)
+			{
+				// debug info
+				g.setColor(Color.MAGENTA);
+				g.drawRect(drawP.x - (int) radius, drawP.y - (int) radius, (int) getDiameter(), (int) getDiameter());
+
+			}
+
+		public void draw(Graphics2D g)
+			{
+				Integer shootsLeft = new Integer(0);
+
+				String shootLeftStr = "";
+
+				modifyTarget();
+
+				// Draw target
+
+				g.setColor(this.c);
+				if (isDead() == false)
+					{
+						g.fillArc(drawP.x - (int) radius, drawP.y - (int) radius, (int) getDiameter(),
+								(int) getDiameter(), 0, 360);
+						g.setColor(Color.black);
+
+						shootsLeft = (hitsToKillTarget - hitsSoFar);
+						shootLeftStr = shootsLeft.toString();
+
+						g.drawString(shootLeftStr, drawP.x - 3, drawP.y + 3);
+					}
+				if (isDead() == true)
+					{
+						g.drawArc(drawP.x - (int) radius, drawP.y - (int) radius, (int) getDiameter(),
+								(int) getDiameter(), 0, 360);
+					}
+
+				// drawDebugInfo(g);
+			}
+
+		private void modifyTarget()
+			{
+				// Change target size
+				if (isDead() == false)
+					{
+						changeSizeOfTarget();
+					}
+				// move target
+				moveTarget();
+
+			}
+
+		public double getDiameter()
+			{
+				return radius * 2.0;
+			}
+
+		public void hit()
+			{
+				hitsSoFar += 1;
+
+				if (hitsSoFar >= hitsToKillTarget)
+					{
+						closeToBorder(true);
+						this.c = Color.blue;
+					}
+
+			}
+
+		public boolean isDead()
+			{
+				if (hitsSoFar >= hitsToKillTarget)
+					{
+
+						return true;
+					} else
+					return false;
+			}
+
+		public void closeToBorder(boolean b)
+			{
+				if (b)
+					{
+						if (hitsSoFar < hitsToKillTarget)
+							this.c = Color.red;
+					}
+			}
+
+		private void init(Point inP, int radius, Color c)
+			{
+				drawP = inP;
+				this.radius = radius;
+				this.c = c;
+
+				// Defaults
+				// no move for target
+				deltaMove = new Point(0, 0);
+				// move size change
+				deltaRadius = 1;
+				minRadius = 10;
+				maxRadius = 200;
+
+			}
+
+		public void isHitEvent(MouseEvent evt)
+			{
+				if (determineIfTargetIsHit(evt.getX() - 8, evt.getY() - 31))
+					{
+						hit();
+					}
+			}
+
+		// public methods
+		public void moveTarget()
+			{
+				// update both points for target
+				drawP.x += deltaMove.x;
+				drawP.y += deltaMove.y;
+
+			}
+
+		public void setDeltaMove(Point deltaMove)
+			{
+				this.deltaMove = deltaMove;
+			}
+
+		public void setDeltaRadius(double d)
+			{
+				this.deltaRadius = d;
+			}
+
+		public void setMaxRadius(int maxRadius)
+			{
+				this.maxRadius = maxRadius;
+			}
+
+		// Setters
+		public void setMinRadius(int minRadius)
+			{
+				this.minRadius = minRadius;
+			}
+
 	}
-
-	static public final int randInt(int min, int max)
-	{
-		Random rand = new Random();
-		if (max <= 0)
-		{
-			max = 1;
-		}
-		int randomNum = rand.nextInt((max - min) + 1) + min;
-		return randomNum;
-	}
-
-	// Constructor
-	public Target(Point inP, int radius, Color c)
-	{
-		super();
-		init(inP, radius, c);
-	}
-
-	// Constructor - no input = random
-	public Target()
-	{
-		// generate random positions
-		c = Color.green;
-		deltaRadius = 1;
-		minRadius = 10;
-		maxRadius = randInt(10, 200);
-		deltaMove = new Point(0, 0);
-
-		drawP = new Point(randInt(300, 1300), randInt(200, 700));
-		if (randInt(1, 10) >= 3)
-		{
-			deltaMove = new Point(randInt(1, 4) - 2, randInt(1, 4) - 2);
-		}
-		radius = randInt(10, 250);
-		if (randInt(1, 10) >= 5)
-		{
-			deltaRadius = 1.0005;
-			maxRadius = randInt(10, 50);
-			;
-		} else
-		{
-			deltaRadius = 0.99;
-		}
-		hitsSoFar = 0;
-		hitsToKillTarget = 1;
-	}
-
-	// Methods
-
-	// Getters
-	public Point getDrawP()
-	{
-		return drawP;
-	}
-
-	public void changeSizeOfTarget()
-	{
-
-		if (deltaRadius != 1)
-		{
-			this.radius *= deltaRadius;
-		}
-		if (radius > maxRadius)
-		{
-			radius = maxRadius;
-		}
-		if (radius < minRadius)
-		{
-			radius = minRadius;
-		}
-
-	}
-
-	public void debugDetermineIfTargetIsHit(int shootX, int shootY, boolean retVal)
-	{
-		System.out.print("Shoot(" + shootX + "," + shootY + ")");
-		if (retVal)
-		{
-			System.out.println(" Hit! Radius :" + radius + " ");
-		} else
-		{
-			System.out.println(" No hit...");
-		}
-
-		System.out.println("Distance : " + drawP.distance(shootY, shootY));
-
-	}
-
-	public boolean determineIfTargetIsHit(int shootX, int shootY)
-	{
-		boolean retVal = false;
-
-		double distance = drawP.distance(shootX, shootY);
-		if (distance < radius)
-		{
-			retVal = true;
-		} else
-		{
-			retVal = false;
-		}
-
-		// debugDetermineIfTargetIsHit(shootX, shootY, retVal);
-
-		return retVal;
-	}
-
-	public void drawDebugInfo(Graphics2D g)
-	{
-		// debug info
-		g.setColor(Color.MAGENTA);
-		g.drawRect(drawP.x - (int) radius, drawP.y - (int) radius, (int) getDiameter(), (int) getDiameter());
-
-	}
-
-	public void draw(Graphics2D g)
-	{
-		modifyTarget();
-
-		// Draw target
-
-		g.setColor(this.c);
-		if (isDead() == false)
-		{
-			g.fillArc(drawP.x - (int) radius, drawP.y - (int) radius, (int) getDiameter(), (int) getDiameter(), 0, 360);
-		}
-		if (isDead() == true)
-		{
-			g.drawArc(drawP.x - (int) radius, drawP.y - (int) radius, (int) getDiameter(), (int) getDiameter(), 0, 360);
-		}
-
-		// drawDebugInfo(g);
-	}
-
-	private void modifyTarget()
-	{
-		// Change target size
-		if (isDead() == false)
-		{
-			changeSizeOfTarget();
-		}
-		// move target
-		moveTarget();
-
-	}
-
-	public double getDiameter()
-	{
-		return radius * 2.0;
-	}
-
-	public void hit()
-	{
-		hitsSoFar += 1;
-
-		if (hitsSoFar >= hitsToKillTarget)
-		{
-			dead(true);
-			this.c = Color.blue;
-		}
-
-	}
-
-	public boolean isDead()
-	{
-		if (hitsSoFar >= hitsToKillTarget)
-		{
-
-			return true;
-		} else
-			return false;
-	}
-
-	public void dead(boolean b)
-	{
-		if (b)
-		{
-			if (hitsSoFar < hitsToKillTarget) this.c = Color.red;
-		}
-	}
-
-	private void init(Point inP, int radius, Color c)
-	{
-		drawP = inP;
-		this.radius = radius;
-		this.c = c;
-
-		// Defaults
-		// no move for target
-		deltaMove = new Point(0, 0);
-		// move size change
-		deltaRadius = 1;
-		minRadius = 10;
-		maxRadius = 200;
-
-	}
-
-	public void isHitEvent(MouseEvent evt)
-	{
-		if (determineIfTargetIsHit(evt.getX() - 8, evt.getY() - 31))
-		{
-			hit();
-		}
-	}
-
-	// public methods
-	public void moveTarget()
-	{
-		// update both points for target
-		drawP.x += deltaMove.x;
-		drawP.y += deltaMove.y;
-
-	}
-
-	public void setDeltaMove(Point deltaMove)
-	{
-		this.deltaMove = deltaMove;
-	}
-
-	public void setDeltaRadius(double d)
-	{
-		this.deltaRadius = d;
-	}
-
-	public void setMaxRadius(int maxRadius)
-	{
-		this.maxRadius = maxRadius;
-	}
-
-	// Setters
-	public void setMinRadius(int minRadius)
-	{
-		this.minRadius = minRadius;
-	}
-
-}
